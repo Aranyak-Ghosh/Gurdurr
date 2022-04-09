fff0091cb3921f1fb74c6cbb4aace267642daeb4
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-04-09 16:09:45 +0400

    WIP query object

---
+++ QueryObject.go
@@ -0,0 +1,88 @@
+package gurdurr
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Aranyak-Ghosh/golist"
+)
+
+type ComparatorType = int
+
+const (
+	GreaterThanOrEqual ComparatorType = iota
+	GreaterThan
+	Equals
+	LessThan
+	LessThanOrEqual
+	NotEquals
+	Like
+	Not
+)
+
+type Clause = int
+
+const (
+	Select Clause = iota
+	Where
+	OrderBy
+	Top
+	Limit
+	Join
+)
+
+type QueryConnector = int
+
+const (
+	AND QueryConnector = iota
+	OR
+)
+
+/***
+SELECT [] <- COLUMNS
+FROM [] <- TABLE_NAME
+JOIN [] <- TABLE_NAME ON ([] <- JOIN_PROPERTY = [] <- JOIN_PROPERTY ) <- COMPARE CLAUSE
+WHERE [] <- COMPARE CLAUSE
+ORDER BY <-
+*/
+
+type queryPart struct {
+	clause     Clause
+	columnName []string
+	comparator []ComparatorType
+	tableName  string
+}
+
+type whereQueryPart struct {
+	queryPart
+	connector *QueryConnector
+}
+
+type queryObject struct {
+	queryString string
+	selector    queryPart
+	whereFilter golist.Queue[whereQueryPart]
+	tableName   string
+}
+
+func (q *queryObject) Select(columns []string) *queryObject {
+	var part queryPart
+
+	part.clause = Select
+	copy(part.columnName, columns)
+
+	selectedColumns := strings.Join(columns, ",")
+
+	selectStatement := fmt.Sprintf("SELECT %s", selectedColumns)
+	q.selector = part
+	q.queryString = fmt.Sprintf("%s %s", selectStatement, q.queryString)
+
+	return q
+}
+
+func NewQuery(tableName string) *queryObject {
+	return &queryObject{
+		tableName:   tableName,
+		queryString: fmt.Sprintf("FROM %s", tableName),
+	}
+}
